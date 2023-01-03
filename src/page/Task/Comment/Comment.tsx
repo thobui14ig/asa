9139d1d2fe4b3526d1425a56e6537e793b5eb97e
thobui14ig <buithanhtho14ig@gmail.com>
2023-01-03 23:57:02 +0700

    cooment

---
+++ src/page/Task/Comment/Comment.tsx
@@ -0,0 +1,11 @@
+export default function Comments(){
+    return(
+        <>
+            <p>comment</p>
+            <p>comment</p>
+
+            <p>comment</p>
+
+        </>
+    )
+}
--- src/page/Task/TaskContent.tsx
+++ src/page/Task/TaskContent.tsx
@@ -1,4 +1,6 @@
 import { ArrowRightOutlined } from '@ant-design/icons';
+import TextArea from 'antd/lib/input/TextArea';
+import Comments from './Comment/Comment';
 import './index.scss';
 
 interface ContentTaskType{
@@ -9,12 +11,20 @@ interface ContentTaskType{
 const TaskContent: React.FC<ContentTaskType> = ({contentTask, setIshowContentTask }) => {
 
     return (
-      <div className="task-content" style={{ width: 700 }}>
+      <div className="task-content">
         <div className='header'>
             <h1 className='mb-6 text-lg'>{contentTask?.name}</h1>
             <ArrowRightOutlined style={{ fontSize: 20 }} onClick={() =>{
                 setIshowContentTask(false)
             }}/>
+        </div>
+        <div className='content'>
+          <div className='comment'>
+            <Comments />
+            <TextArea rows={4}  />
+            <button className='button-send-comment'>submit</button>
+          </div>
+
         </div>
         
       </div>
